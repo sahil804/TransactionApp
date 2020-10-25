8adf40af6127167c1fdb9f4f0c05be50564c4b7d
Sahil Suri <sahilsuridev@gmail.com>
2020-10-26 07:51:30 +1100

    removed unwanted dependency

--- app/src/main/java/com/example/cbasample/data/model/TransactionResponseData.kt
+++ app/src/main/java/com/example/cbasample/data/model/TransactionResponseData.kt
@@ -1,10 +1,8 @@
 package com.example.cbasample.data.model
 
 import android.os.Parcelable
-import androidx.room.Entity
 import kotlinx.android.parcel.Parcelize
 
-@Entity
 data class TransactionResponseData (
     val account : Account,
     val transactions : List<Transaction>,
